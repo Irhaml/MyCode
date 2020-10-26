820551482a31891d5d2c4b89db209a6bfd12740c
Irham <irhamkuro92@gmail.com>
2020-10-26 21:17:04 +0700

    Perubahan ke

--- AutoPull.cmd
+++ AutoPull.cmd
@@ -3,6 +3,6 @@ git remote add origin https://github.com/Irhaml/MyCode
 git add .
 set /p count=<Counter.txt
 set /a count+=1
-echo %a% > Counter.txt
+echo /p %a% > Counter.txt
 git commit -m "Perubahan ke "
 git push origin master
