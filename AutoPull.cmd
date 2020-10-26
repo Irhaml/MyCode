8017b2198486e7cf7a77f329fa57c558d5c55e00
Irham <irhamkuro92@gmail.com>
2020-10-26 21:52:07 +0700

    reset

--- AutoPull.cmd
+++
@@ -1,9 +0,0 @@
-git init
-git remote add origin https://github.com/Irhaml/MyCode
-git add .
-set /p count=<Counter.txt
-set /a count+=1
-echo %count% > Counter.txt
-echo %count%
-git commit -m "Irham -> perubahan ke %count%"
-git push origin master
