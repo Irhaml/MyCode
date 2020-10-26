33b1dc993bb7e8d03009e5d63e748b9cc225c42e
Irham <irhamkuro92@gmail.com>
2020-10-27 01:06:11 +0700

    Irham -> perubahan ke 2

---
+++ Hackerrank/Python/Python_Loops.py
@@ -0,0 +1,14 @@
+
+#//////////////////////////////////////////////////////#
+#/ This program was written by Muhammad Irham Luthfi. /#
+#/ Intended for educational use.                      /#
+#/ No rights reserved.                                /#
+#//////////////////////////////////////////////////////#
+
+
+n = int(input())
+
+for i in range(n):
+    print(i**2)
+
+
